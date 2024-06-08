b60063345a8aea527b2589737d650d3579f7f289
Programmer-Masirul <saidhstu@gmail.com>
2024-06-08 22:19:04 +0600

    modified actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# Variables (replace these with your actual values)
+DB_DATABASE="${DB_DATABASE}"
+DB_USERNAME="${DB_USERNAME}"
+DB_PASSWORD="${DB_PASSWORD}"
+
 # Install PHP and Composer if not installed
 if ! command -v php &> /dev/null; then
     sudo apt-get update
-    sudo apt-get install -y php php-cli php-mbstring unzip
+    sudo apt-get install -y php php-cli php-mbstring unzip php-xml
 fi
 
 if ! command -v composer &> /dev/null; then
@@ -17,7 +22,11 @@ if ! command -v mysql &> /dev/null; then
     sudo service mysql start
 fi
 
-
+# Configure MySQL
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO 'root'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
 
 # Navigate to the project directory
 cd /var/www/html/laravelcicd
@@ -28,13 +37,13 @@ if [ -f composer.lock ]; then
     LOCK_HASH_REMOTE=$(ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no ${SSH_USERNAME}@${SSH_HOST} "md5sum /var/www/html/laravelcicd/composer.lock | awk '{ print $1 }'")
     if [ "$LOCK_HASH_LOCAL" != "$LOCK_HASH_REMOTE" ]; then
         echo "composer.lock has changed. Installing dependencies..."
-        composer install --prefer-dist --no-progress --ignore-platform-req=ext-curl --ignore-platform-req=ext-dom --ignore-platform-req=ext-xml
+        composer install --prefer-dist --no-progress
     else
         echo "composer.lock has not changed. Skipping composer install."
     fi
 else
     echo "composer.lock not found. Installing dependencies..."
-    composer install --prefer-dist --no-progress --ignore-platform-req=ext-curl --ignore-platform-req=ext-dom --ignore-platform-req=ext-xml
+    composer install --prefer-dist --no-progress
 fi
 
 # Run database migrations
