9df6efbbae25043c17892ea970d2dca1d9a53cf1
Programmer-Masirul <saidhstu@gmail.com>
2024-06-10 12:01:34 +0600

    github issue fixed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,6 @@ echo "Deployment started ..."
 # if already is in maintenance mode
 (php artisan down) || true
 
-# Pull the latest version of the app
-git reset --hard
-git pull origin master
-
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
